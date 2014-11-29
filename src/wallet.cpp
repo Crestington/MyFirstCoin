b23b657dd230796b5f9b4072458d04056400f7c5
Crestington <Zelus2009@hotmail.com>
2014-11-29 14:54:58 -0800

    combine thresholds

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -1373,9 +1373,9 @@ bool CWallet::CreateCoinStake(const CKeyStore& keystore, unsigned int nBits, int
 {
     // The following split & combine thresholds are important to security
     // Should not be adjusted if you don't understand the consequences
-    static unsigned int nStakeSplitAge = (60 * 60 * 24 * 30);
+    static unsigned int nStakeSplitAge = (60 * 60 * 24 * 5);
 	const CBlockIndex* pIndex0 = GetLastBlockIndex(pindexBest, false);
-    int64 nCombineThreshold = 0;
+    int64 nCombineThreshold = 1000;
 	if(pIndex0->pprev)
 		nCombineThreshold = GetProofOfWorkReward(pIndex0->nHeight, MIN_TX_FEE, pIndex0->pprev->GetBlockHash()) / 3;
 
